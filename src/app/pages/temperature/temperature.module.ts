ffa21ca5525aa90d77c7fbcfcf075362e70fdc8c
Hallan Pierre <hallanpierre@gmail.com>
2020-11-14 14:33:52 -0300

    Add temperature page

---
+++ src/app/pages/temperature/temperature.module.ts
@@ -0,0 +1,23 @@
+import { ChartModule } from 'angular2-highcharts';
+import * as highcharts from 'highcharts';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { TemperaturePageRoutingModule } from './temperature-routing.module';
+
+import { TemperaturePage } from './temperature.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    TemperaturePageRoutingModule,
+    ChartModule.forRoot(highcharts)
+  ],
+  declarations: [TemperaturePage]
+})
+export class TemperaturePageModule {}
